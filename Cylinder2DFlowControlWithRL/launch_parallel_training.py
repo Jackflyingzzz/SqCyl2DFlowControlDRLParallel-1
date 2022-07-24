42cad465290059729fe5ea9b259da677a2fe6cfa
Jackflyingzzz <105610300+Jackflyingzzz@users.noreply.github.com>
2022-07-24 17:25:57 +0100

    Update launch_parallel_training.py

--- Cylinder2DFlowControlWithRL/launch_parallel_training.py
+++ Cylinder2DFlowControlWithRL/launch_parallel_training.py
@@ -37,7 +37,9 @@ for crrt_simu in range(number_servers):
         timing_print=(crrt_simu == 0)     # Only print time info for env_0
     ))
 
-network = [dict(type='retrieve', tensors = ['obs']), dict(type='rnn', size=512, horizon=200,cell='lstm')]
+network = [dict(type='input_rnn', size = 512, cell='gru]), 
+           dict(type='rnn', size=512, horizon=50,cell='gru'),
+           dict(type='rnn', size=512, horizon=50,cell='gru'),]
 
 agent = Agent.create(
     # Agent + Environment
